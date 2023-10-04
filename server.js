380eeeb0cca3b04724f7b9b49a9f90f147b43cba
ersinev <ersinevci@hotmail.com>
2023-10-04 15:59:25 +0200

    updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const rooms = {};
 const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with the URL of your frontend
+  origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 app.use(cors(corsOptions));
