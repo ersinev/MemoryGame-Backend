63d57468fc6caefd2363aec6647b1dd39bcabb6c
ersinev <ersinevci@hotmail.com>
2023-10-05 15:45:59 +0200

    updated

--- server.js
+++ server.js
@@ -1,71 +1,92 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000', // Replace with your frontend URL
     methods: ['GET', 'POST'],
   },
 });
-const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const rooms = {};
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000', // Replace with your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 app.use(cors(corsOptions));
-let totalUsers= 0
+
+let totalUsers = 0;
+
 io.on('connection', (socket) => {
   console.log('A user connected');
   totalUsers++;
-  console.log(`---------Total Online Users: ${totalUsers}--------------`)
+  console.log(`---------Total Online Users: ${totalUsers}--------------`);
 
   socket.on('join-room', (roomId, playerName) => {
     socket.join(roomId);
-    
+
     if (!rooms[roomId]) {
-      rooms[roomId] = [];
+      rooms[roomId] = {
+        players: [],
+        gameId: generateUniqueId(roomId),
+        gameStarted: false,
+      };
     }
-    
-   
-    rooms[roomId].push({ id: socket.id, name: playerName });
-    
-    io.to(roomId).emit('player-joined', rooms[roomId]); 
+
+    rooms[roomId].players.push({ id: socket.id, name: playerName });
+
+    io.to(roomId).emit('player-joined', rooms[roomId].players);
+
+    if (rooms[roomId].players.length >= 2 && !rooms[roomId].gameStarted) {
+      rooms[roomId].gameStarted = true;
+      io.to(roomId).emit('game-started', rooms[roomId].gameId);
+      // Implement game start logic here (e.g., shuffle and distribute cards)
+    }
   });
 
   socket.on('start-game', (roomId) => {
-    // Handle game start logic here
-    // You can emit a 'game-started' event to all players in the room
-    io.to(roomId).emit('game-started');
+    if (rooms[roomId]) {
+      io.to(roomId).emit('game-started', rooms[roomId].gameId);
+      // Implement game start logic here (e.g., shuffle and distribute cards)
+      console.log(roomId)
+    } else {
+      console.log(`Room with ID ${roomId} does not exist.`);
+    }
   });
 
-  socket.on('flip-card', (roomId, cardId) => {
-    // Handle when a player flips a card
-    // You can broadcast the card flip to all players in the room
-    io.to(roomId).emit('flip-card', cardId);
+  socket.on('flip-card', (roomId, playerName, cardId) => {
+    io.to(roomId).emit('card-flipped', playerName, cardId);
   });
 
   socket.on('disconnect', () => {
-    totalUsers--
+    totalUsers--;
 
     console.log('A user disconnected');
-    console.log(`---------Total Online Users: ${totalUsers}--------------`)
+    console.log(`---------Total Online Users: ${totalUsers}--------------`);
+
     const roomId = Object.keys(socket.rooms)[1];
     if (roomId && rooms[roomId]) {
-      const playerIndex = rooms[roomId].findIndex((player) => player.id === socket.id);
+      const playerIndex = rooms[roomId].players.findIndex(
+        (player) => player.id === socket.id
+      );
       if (playerIndex !== -1) {
-        rooms[roomId].splice(playerIndex, 1);
-        io.to(roomId).emit('player-left', rooms[roomId]);
+        rooms[roomId].players.splice(playerIndex, 1);
+        io.to(roomId).emit('player-left', rooms[roomId].players);
       }
     }
   });
 });
+
+function generateUniqueId(roomId) {
+  // Implement your logic to generate unique IDs here
+  return roomId + '_gameId';
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
