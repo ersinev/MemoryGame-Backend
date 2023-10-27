67167d7a4d86046ed3dacd1ebbffdce0d96ab812
ersinev <ersinevci@hotmail.com>
2023-10-27 15:50:32 +0200

    updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: "http://localhost:3000", // frontend URL
     methods: ["GET", "POST"],
   },
 });
