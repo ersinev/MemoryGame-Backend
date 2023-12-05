fa18cb3eca8ac45df893a7af50e75520a4e4517a
ersinev <ersinevci@hotmail.com>
2023-12-05 13:29:50 +0100

    updated

--- server.js
+++ server.js
@@ -115,8 +115,9 @@ io.on("connection", (socket) => {
     console.log("SELECTED CARDS",selectedCards)
   
     // Broadcast the flip event to all clients in the same room,
-    // including the sender
-    io.to(roomId).emit("flip-card", playerName, cardId);
+    // including the sender 
+    
+    io.to(roomId).emit("flip-card", playerName, cardId, selectedCards);
   });
   
 
